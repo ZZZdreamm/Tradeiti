f095bb12c3956ad34a780a809460d7c5fcc85736
kmultan <01178567@pw.edu.pl>
2023-11-10 16:10:05 +0100

    Remove test iframe from ForLecturers

--- frontend/src/pages/forLecturers/ForLecturers.tsx
+++ frontend/src/pages/forLecturers/ForLecturers.tsx
@@ -5,15 +5,6 @@ const ForLecturers = () => {
     <div>
       <h1>Dla wykładowcy</h1>
       <br />
-      <iframe
-        width="853"
-        height="480"
-        src={`https://www.youtube.com/embed/PjZi1zlxXRA`}
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        title="Embedded youtube"
-      />
     </div>
   );
 };
