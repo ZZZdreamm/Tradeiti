bda2ae0266e00795e1c579d205a8bde03fabf003
akempins <01178535@pw.edu.pl>
2023-11-11 10:17:09 +0100

    Fixed import

--- backend/src/main/java/com/example/usos_oauth/controller/CourseEditionsController.java
+++ backend/src/main/java/com/example/usos_oauth/controller/CourseEditionsController.java
@@ -1,6 +1,9 @@
 package com.example.usos_oauth.controller;
 
+import com.example.usos_oauth.usos.api.Usos;
 import com.example.usos_oauth.usos.api.impl.CourseEdition;
+import com.example.usos_oauth.usos.connect.UsosServiceProvider;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("api/course-editions")
 public class CourseEditionsController {
+
+    private UsosServiceProvider usosServiceProvider;
 
     @GetMapping
     public List<CourseEdition> getCourseEditions(@RequestParam("access_token") String accessToken,
