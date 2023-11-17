59b549c4d618ebfbe31fffcd1ecc341ba492cbbc
ZZZdream <kmultan1234@gmail.com>
2023-11-17 18:05:05 +0100

    Add searching work

---
+++ frontend/src/apiFunctions/searchOffers.ts
@@ -0,0 +1,11 @@
+import { axiosBase } from "../config/axiosConfig";
+import { mockedOffers } from "../mocks/MockedOffers";
+import { SearchOfferOptions } from "../models/SearchOfferOptions";
+
+export async function searchOffers(searchValues: SearchOfferOptions) {
+  return await new Promise<{ data: any }>((res, _) => {
+    res({ data: mockedOffers.filter((offer) => offer.status === "pending") });
+  });
+
+  return await axiosBase.post("/search-offers", searchValues);
+}
--- frontend/src/pages/offers/Offers.tsx
+++ frontend/src/pages/offers/Offers.tsx
@@ -2,8 +2,10 @@ import { useQuery } from "react-query";
 import { withPrivateRoute } from "../../common/withPrivateRoute/WithPrivateRoute";
 import { OffersList } from "../../components/offer/OffersList";
 import { SearchTable } from "../../components/searchTable/SearchTable";
-import { mockedOffers } from "../../mocks/MockedOffers";
 import "./style.scss";
+import { getOffers } from "../../apiFunctions/getOffers";
+import { searchOffers } from "../../apiFunctions/searchOffers";
+import { useState } from "react";
 
 const searchTableInputsLabels = [
   { label: "Offer ID", input: "offer_id" },
@@ -13,14 +15,23 @@ const searchTableInputsLabels = [
 ];
 
 const Offers = () => {
-  const {} = useQuery("offers", () => {});
+  const { data: offers } = useQuery("offers", getOffers);
+  const [searchedOffers, setSearchedOffers] = useState([]);
+
+  const handleSearch = async (values: any) => {
+    const { data } = await searchOffers(values);
+    setSearchedOffers(data);
+  };
+
   return (
     <div className="offers">
       <SearchTable
         inputsLabels={searchTableInputsLabels}
-        handleOnSubmit={() => {}}
+        handleOnSubmit={handleSearch}
       />
-      <OffersList offers={mockedOffers} />
+      <OffersList
+        offers={searchedOffers.length > 0 ? searchedOffers : offers?.data}
+      />
     </div>
   );
 };
