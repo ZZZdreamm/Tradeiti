68aa961f397874a4e80b8628434cc30ae8f0c598
ZZZdream <kmultan1234@gmail.com>
2023-11-19 17:36:22 +0100

    Include jwtToken in axios requests

--- frontend/src/config/axiosConfig.ts
+++ frontend/src/config/axiosConfig.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
 
-let apiBaseUrl = "https://www.arcziweb.com/api/"
+let apiBaseUrl = "https://www.arcziweb.com/api/";
 
-if(process.env.NODE_ENV === "development") {
-    apiBaseUrl =  "http://localhost:8091/api/"
+if (process.env.NODE_ENV === "development") {
+  apiBaseUrl = "http://localhost:8090/api/";
 }
 
 export const axiosBase = axios.create({
-    baseURL: apiBaseUrl
-})
+  baseURL: apiBaseUrl,
+  headers: {
+    Authorization: "Bearer " + localStorage.getItem("jwtToken"),
+  },
+});
