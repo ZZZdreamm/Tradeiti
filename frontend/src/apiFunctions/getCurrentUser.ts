238ecfd0a9d07ffc6940bf0fcd411873812147f8
ZZZdream <kmultan1234@gmail.com>
2023-11-24 13:49:58 +0100

    Check if user connected to usos

--- frontend/src/apiFunctions/getCurrentUser.ts
+++ frontend/src/apiFunctions/getCurrentUser.ts
@@ -1,9 +1,5 @@
 import { axiosBase } from "../config/axiosConfig";
-import { ACCESS_TOKEN } from "../config/constants";
 
 export function getCurrentUser() {
-  if (!localStorage.getItem(ACCESS_TOKEN)) {
-    return Promise.reject("No access token set.");
-  }
-  return axiosBase.get("/course-editions");
+  return axiosBase.get("/auth/test-token");
 }
