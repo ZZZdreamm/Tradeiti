903cb3cfb9dbc68ef9870913f5c511a9a4e405f0
ZZZdream <kmultan1234@gmail.com>
2023-11-24 15:08:15 +0100

    Add boolean | undefined type

--- frontend/src/hooks/useCurrentUser.tsx
+++ frontend/src/hooks/useCurrentUser.tsx
@@ -11,7 +11,7 @@ export function useCurrentUser() {
   const [authenticated, setAuthenticated] = useState(false);
   const [loading, setLoading] = useState(true);
   const [mounted, setMounted] = useState(false);
-  const [connectedToUsos, setConnectedToUsos] = useState<boolean | undefined>();
+  const [connectedToUsos, setConnectedToUsos] = useState<boolean | undefined>(undefined);
 
   useEffect(() => {
     setMounted(true);
