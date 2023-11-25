2d68c0563a98d85eb029c2d3e507a7a6bd65ffe9
akempins <01178535@pw.edu.pl>
2023-11-25 10:58:52 +0100

    directory restructuring

---
+++ backend/src/main/java/com/example/usos_oauth/usos/config/OAuthConfig.java
@@ -0,0 +1,20 @@
+package com.example.usos_oauth.usos.config;
+
+import com.example.usos_oauth.usos.connect.UsosServiceProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OAuthConfig {
+
+    @Value("${usos.consumer.key}")
+    private String consumerKey;
+    @Value("${usos.consumer.secret}")
+    private String consumerSecret;
+
+    @Bean
+    public UsosServiceProvider usosServiceProvider() {
+        return new UsosServiceProvider(consumerKey, consumerSecret);
+    }
+}
