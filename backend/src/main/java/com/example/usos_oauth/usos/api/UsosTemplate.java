95289378552d21a865febc5fc02db4366564598b
akempins <01178535@pw.edu.pl>
2023-11-27 13:52:46 +0100

    Changed UsosTemplate

--- backend/src/main/java/com/example/usos_oauth/usos/api/UsosTemplate.java
+++ backend/src/main/java/com/example/usos_oauth/usos/api/UsosTemplate.java
@@ -8,11 +8,13 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 public class UsosTemplate extends AbstractOAuth1ApiBinding {
 
     URI courseEditionsUri = URI.create("https://apps.usos.pw.edu.pl/services/courses/user");
     URI userUri = URI.create("https://apps.usos.pw.edu.pl/services/users/user");
+    URI userGroups = URI.create("https://apps.usos.pw.edu.pl/services/tt/course_edition");
 
     public UsosTemplate(String consumerKey, String consumerSecret, String accessToken, String secret) {
         super(consumerKey, consumerSecret, accessToken, secret);
@@ -23,8 +25,21 @@ public class UsosTemplate extends AbstractOAuth1ApiBinding {
         CourseResponse response = restTemplate.getForObject(courseEditionsUri, CourseResponse.class);
         return response.getCourse_editions().get(Term.getAcademicTerm());
     }
+
     public UsosUser getUser() {
         RestTemplate restTemplate = getRestTemplate();
         return restTemplate.getForObject(userUri, UsosUser.class);
+    }
+
+    public List<Activity> getUserGroups(Map<String, String> params){
+      RestTemplate restTemplate = getRestTemplate();
+      URI uri = UriComponentsBuilder.fromUri(userGroups)
+        .queryParam("course_id", params.get("course_id"))
+        .queryParam("term_id", params.get("term_id"))
+        .queryParam("fields", "name|start_time|end_time|course_name|lecturer_ids|classtype_name")
+        .build()
+        .toUri();
+      GroupsResponse response = restTemplate.getForObject(uri, GroupsResponse.class);
+      return response.getActivities();
     }
 }
