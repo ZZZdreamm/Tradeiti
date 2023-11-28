5d5abf34afeae7e5110de156fb2ceeac159e8136
akempins <01178535@pw.edu.pl>
2023-11-28 12:02:28 +0100

    Added custom exception for usosService

---
+++ backend/src/main/java/com/example/usos_oauth/usos/service/exception/UsosAccountNotConnectedException.java
@@ -0,0 +1,9 @@
+package com.example.usos_oauth.usos.service.exception;
+
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@NoArgsConstructor
+@ResponseStatus(HttpStatus.PROXY_AUTHENTICATION_REQUIRED)
+public class UsosAccountNotConnectedException extends RuntimeException {}
