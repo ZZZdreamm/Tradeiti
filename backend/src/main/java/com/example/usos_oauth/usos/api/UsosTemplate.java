1768cfef67520b65307ed2739442cb569adbe958
oszypczy <01178608@pw.edu.pl>
2023-11-28 01:02:48 +0100

    backend/new endpoint - getting all groups of given course - without wykłady.
    Still i have to implement better info about lecturer and about day of the week.

--- backend/src/main/java/com/example/usos_oauth/usos/api/UsosTemplate.java
+++ backend/src/main/java/com/example/usos_oauth/usos/api/UsosTemplate.java
@@ -1,12 +1,16 @@
 package com.example.usos_oauth.usos.api;
 
 import com.example.usos_oauth.usos.api.logic.Term;
-import com.example.usos_oauth.usos.api.model.*;
+import com.example.usos_oauth.usos.api.model.Activity;
+import com.example.usos_oauth.usos.api.model.CourseEdition;
+import com.example.usos_oauth.usos.api.model.CourseResponse;
+import com.example.usos_oauth.usos.api.model.UsosUser;
 import org.springframework.social.oauth1.AbstractOAuth1ApiBinding;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -23,12 +27,13 @@ public class UsosTemplate extends AbstractOAuth1ApiBinding {
     public List<CourseEdition> getCourseEditions() {
         RestTemplate restTemplate = getRestTemplate();
         CourseResponse response = restTemplate.getForObject(courseEditionsUri, CourseResponse.class);
+        assert response != null;
         return response.getCourse_editions().get(Term.getAcademicTerm());
     }
 
-    public UsosUser getUser() {
+    public void getUser() {
         RestTemplate restTemplate = getRestTemplate();
-        return restTemplate.getForObject(userUri, UsosUser.class);
+        restTemplate.getForObject(userUri, UsosUser.class);
     }
 
     public List<Activity> getUserGroups(Map<String, String> params){
@@ -39,7 +44,32 @@ public class UsosTemplate extends AbstractOAuth1ApiBinding {
         .queryParam("fields", "name|start_time|end_time|course_name|lecturer_ids|classtype_name")
         .build()
         .toUri();
-      GroupsResponse response = restTemplate.getForObject(uri, GroupsResponse.class);
-      return response.getActivities();
+      Activity[] response = restTemplate.getForObject(uri, Activity[].class);
+
+      assert response != null;
+      List<Activity> filteredResponse = Arrays.stream(response)
+          .filter(entry -> !"Wykład".equals(entry.getClasstype_name().getPl()))
+          .toList();
+
+//      for (Activity activity : response) {
+//          updateLecturerInfo(activity);
+//      }
+
+      return filteredResponse;
+    }
+
+    private void updateLecturerInfo(Activity activity) {
+        RestTemplate restTemplate = getRestTemplate();
+        List<Long> lecturerIds = activity.getLecturer_ids();
+        URI uri = UriComponentsBuilder.fromUri(userUri)
+                .queryParam("user_id", lecturerIds)
+                .build()
+                .toUri();
+        for (int i = 0; i < lecturerIds.size(); i++) {
+//          W tym miejscu wali mi 401 - Unauthorized tak jakby jwt nie działał pomocy
+            UsosUser user = restTemplate.getForObject(uri, UsosUser.class);
+            assert user != null;
+            activity.getLecturer_names().add(user.getFirst_name() + " " + user.getLast_name());
+        }
     }
 }
