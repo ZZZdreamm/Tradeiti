8a57b33429c5b2496466b0750879b0d6a8089275
Jakub Kryczka <kryczka.jakub.pg2@gmail.com>
2023-11-28 23:21:41 +0100

    frontEnd/offerAddingStyles

--- frontend/src/components/courseDate/CourseDate.tsx
+++ frontend/src/components/courseDate/CourseDate.tsx
@@ -1,6 +1,6 @@
 import { CourseDate } from "../../models/CourseDate";
 import "./CourseDate.scss"
-// import {useState} from 'react'
+  // import {useState} from 'react'
 
 interface CourseDateProps {
   date: CourseDate;
@@ -23,15 +23,31 @@ export function CourseDateComponent({
   };
 
   const handleSpanClick = (event: React.MouseEvent<HTMLSpanElement>) => {
+    if(event.currentTarget && event.currentTarget.classList.contains('unavailable')){
+      return;
+    }
     const radioInput = event.currentTarget.querySelector('input[type="radio"]');
     const spans = getAllElementsByClassName(event.currentTarget.classList[1]);
     if (radioInput) {
-      (radioInput as HTMLElement).click(); // Trigger a click on the radio input
+      (radioInput as HTMLElement).click();
       for(let i = 0; i < spans.length; i++){
         (spans[i] as HTMLElement).style.borderRadius = '0px';
+        if(spans[i] == event.currentTarget && event.currentTarget.classList.contains('myHour')){
+          const oppSpans = getAllElementsByClassName('opponentHour');
+          for(let o = 0; o < oppSpans.length; o++){
+            if((oppSpans[o] as HTMLElement).classList.contains('unavailable')){
+              (oppSpans[o] as HTMLElement).classList.remove('unavailable');
+              (oppSpans[o].firstChild as HTMLInputElement).disabled = false;
+            }
+          }
+          (oppSpans[i] as HTMLElement).classList.add('unavailable');
+          (oppSpans[i] as HTMLElement).style.borderRadius = '0px';
+          (oppSpans[i].firstChild as HTMLInputElement).disabled = true;
+          (oppSpans[i].firstChild as HTMLInputElement).checked = false;
+        }
       }
       event.currentTarget.style.borderRadius = '15px';
-      handleChooseDate(date, hourType); // Call your custom handler if needed
+      handleChooseDate(date, hourType);
     }
   };
 
