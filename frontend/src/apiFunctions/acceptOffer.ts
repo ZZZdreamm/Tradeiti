cfa9b3bd54aa3ff0c5d3bbcd7cdc5de6785cd461
ZZZdream <kmultan1234@gmail.com>
2023-11-29 17:49:34 +0100

    Change acceptOffer

--- frontend/src/apiFunctions/acceptOffer.ts
+++ frontend/src/apiFunctions/acceptOffer.ts
@@ -1,10 +1,8 @@
 import { axiosBase } from "../config/axiosConfig";
 
-export function acceptOffer(offer_id: string, accepter_id: string) {
+export function acceptOffer(offer_id: string) {
   return new Promise<{ data: any; status: number }>((res, _) => {
     res({ data: "mocked value", status: 200 });
   });
-  return axiosBase.patch(
-    `/offers/accept?offer_id=${offer_id}&accepter_id=${accepter_id}`
-  );
+  return axiosBase.patch(`/offers/accept?offer_id=${offer_id}`);
 }
