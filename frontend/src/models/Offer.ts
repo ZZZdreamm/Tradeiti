13e062ced3092156ebfc2bf0268759f632f7cb45
ZZZdream <kmultan1234@gmail.com>
2023-11-29 17:51:59 +0100

    Add CourseDateData interface

--- frontend/src/models/Offer.ts
+++ frontend/src/models/Offer.ts
@@ -4,8 +4,8 @@ import { OfferStatus } from "./OfferStatus";
 export interface Offer {
   offer_id: string;
   course_name: string;
-  selled_date_data: CourseDateData[];
-  exchanged_date_data: CourseDateData[];
+  selled_date_data: CourseDateData;
+  exchanged_date_data: CourseDateData;
   requester_id: string;
   accepter_id: string;
   status: OfferStatus;
