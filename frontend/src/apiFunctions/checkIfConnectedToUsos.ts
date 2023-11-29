46e411224c8f1ae5638f4ac12ba6daa5e5864f21
ZZZdream <kmultan1234@gmail.com>
2023-11-29 15:42:43 +0100

    Use axios in checkIfConnectedToUsos

--- frontend/src/apiFunctions/checkIfConnectedToUsos.ts
+++ frontend/src/apiFunctions/checkIfConnectedToUsos.ts
@@ -1,18 +1,17 @@
-// import { axiosBase } from "../config/axiosConfig";
-
-import { apiBaseUrl } from "../config/axiosConfig";
+import { axiosBase } from "../config/axiosConfig";
 import { JWT_TOKEN } from "../config/constants";
 
 export function checkIfConnectedToUsos() {
-  return fetch(`${apiBaseUrl}/usos/check-connection`, {
-    headers: {
-      Authorization: "Bearer " + localStorage.getItem(JWT_TOKEN),
-    },
-  }).then((response) => {
-    if(response.status > 400) {
-      throw new Error("Not connected to USOS");
-    }
-    return response.text();
-  });
-
+  return axiosBase
+    .get(`/usos/check-connection`, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem(JWT_TOKEN),
+      },
+    })
+    .then((response) => {
+      if (response.status > 400) {
+        throw new Error("Not connected to USOS");
+      }
+      return response.data;
+    });
 }
