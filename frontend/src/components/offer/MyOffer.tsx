1d4d09377d1f9eb1b45b09d7333168392024035f
ZZZdream <kmultan1234@gmail.com>
2023-11-29 17:54:25 +0100

    Adjust MyOffer component to new Offer model

--- frontend/src/components/offer/MyOffer.tsx
+++ frontend/src/components/offer/MyOffer.tsx
@@ -1,7 +1,8 @@
 import { acceptOffer } from "../../apiFunctions/acceptOffer";
-import { discardOffer } from "../../apiFunctions/discardOffer";
-// import { useCurrentUser } from "../../hooks/useCurrentUser";
+import { rejectOffer } from "../../apiFunctions/rejectOffer";
+import { removeOffer } from "../../apiFunctions/removeOffer";
 import { Offer } from "../../models/Offer";
+import { OfferStatus } from "../../models/OfferStatus";
 import "./style.scss";
 
 interface Props {
@@ -9,18 +10,18 @@ interface Props {
 }
 
 export function MyOfferComponent({ offer }: Props) {
-  // const { currentUser } = useCurrentUser();
   const handleApproveOffer = () => {
-    acceptOffer(
-      // offer.offer_id, currentUser?.id
-      ).then((res) => {
+    acceptOffer(offer.offer_id).then((res) => {
       console.log(res);
     });
   };
-  const handleDiscardOffer = () => {
-    discardOffer(
-      // offer.offer_id, currentUser?.id
-      ).then((res) => {
+  const handleRemoveOffer = () => {
+    removeOffer(offer.offer_id).then((res) => {
+      console.log(res);
+    });
+  };
+  const handleRejectOffer = () => {
+    rejectOffer(offer.offer_id).then((res) => {
       console.log(res);
     });
   };
@@ -29,22 +30,34 @@ export function MyOfferComponent({ offer }: Props) {
       <div className="offer-left">
         <h6>{offer.offer_id}</h6>
         <h6>{offer.course_name}</h6>
-        <span>{offer.lecturer}</span>
-        <span>{offer.start_time}</span>
+        <span>{offer.selled_date_data.lecturer}</span>
+        <span>
+          {offer.selled_date_data.course_day}{" "}
+          {offer.selled_date_data.course_time}
+        </span>
       </div>
       <div className="offer-right">
-        <p>Status: {offer.status === 'accepted' ? 'accepted ✔️' : offer.status}</p>
+        <p>
+          Status:{" "}
+          {offer.status === OfferStatus.accepted ? "accepted ✔️" : offer.status}
+        </p>
         <button
-          className = "approveButton"
+          className="approveButton"
           onClick={handleApproveOffer}
-          disabled={offer.status !== 'accepted'}>
-            Zatwierdź ofertę</button>
-            <button
-          className = "approveButton"
-          onClick={handleDiscardOffer}
-          >{offer.status === 'accepted' ? 'Odrzuć ofertę' : 'Wycofaj ofertę'}
-            </button>
-
+          disabled={offer.status !== OfferStatus.accepted}
+        >
+          Zatwierdź ofertę
+        </button>
+        {offer.status === OfferStatus.accepted && (
+          <button className="approveButton" onClick={handleRejectOffer}>
+            Odrzuć ofertę
+          </button>
+        )}
+        {offer.status === OfferStatus.pending && (
+          <button className="approveButton" onClick={handleRemoveOffer}>
+            Wycofaj ofertę
+          </button>
+        )}
       </div>
     </div>
   );
