f2eba0736bc52f8af4bbf472b349a0cd6f492361
ZZZdream <kmultan1234@gmail.com>
2023-11-30 10:39:22 +0100

    Handle adding offer

--- frontend/src/pages/myOffers/AddOffer.tsx
+++ frontend/src/pages/myOffers/AddOffer.tsx
@@ -1,4 +1,4 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import "./AddOffer.scss";
 import { ChooseCourse } from "./ChooseCourse";
 import { ChooseCourseHour } from "./ChooseCourseHour";
@@ -7,8 +7,12 @@ import { FinalizeOffer } from "./FinalizeOffer";
 import { getDefaultOfferFormValues } from "./DefaultOfferFormValues";
 import { useEffect } from "react";
 import { MyOffersSteps } from "./MyOffersSteps";
+import { OfferCreationDto } from "../../models/OfferCreationDto";
+import { addOffer } from "../../apiFunctions/addOffer";
+import { FormOffer } from "../../models/form/FormOffer";
 
 export function AddOffer() {
+  const navigate = useNavigate();
   const methods = useForm({
     defaultValues: getDefaultOfferFormValues(),
   });
@@ -17,10 +21,33 @@ export function AddOffer() {
 
   const { handleSubmit } = methods;
 
-  const onSubmit: SubmitHandler<any> = (values) => {
-    console.log(values);
+  const onSubmit: SubmitHandler<FormOffer> = (values) => {
+    const createdOffer: OfferCreationDto = {
+      course_id: values.course.course_id,
+      selled_date_data: {
+        course_day: values.myHour.course_day,
+        course_time: values.myHour.course_time,
+        lecturer: values.myHour.lecturer,
+      },
+      exchanged_date_data: {
+        course_day: values.opponentHour.course_day,
+        course_time: values.opponentHour.course_time,
+        lecturer: values.opponentHour.lecturer,
+      },
+    };
+    console.log(createdOffer);
+    addOffer(createdOffer)
+      .then((response) => {
+        console.log(response);
+        alert("Oferta została dodana");
+        navigate(0);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Wystąpił błąd");
+        navigate(0);
+      });
   };
-
 
   useEffect(() => {
     const path = window.location.href;
