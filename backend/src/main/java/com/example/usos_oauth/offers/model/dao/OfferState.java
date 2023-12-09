ce25a3eb7805190a469e8ee9a61d8e10908a45c5
akempins <01178535@pw.edu.pl>
2023-12-09 14:54:57 +0100

    removed OfferStateSerializer.java

--- backend/src/main/java/com/example/usos_oauth/offers/model/dao/OfferState.java
+++ backend/src/main/java/com/example/usos_oauth/offers/model/dao/OfferState.java
@@ -1,9 +1,5 @@
 package com.example.usos_oauth.offers.model.dao;
 
-import com.example.usos_oauth.offers.service.utils.OfferStateSerializer;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
-@JsonSerialize(using = OfferStateSerializer.class)
 public enum OfferState {
     PENDING,
     REQUEST_SENT,
