ea79c5879942a58f3e23548dbc4c0eeb04f7d4d4
ZZZdream <kmultan1234@gmail.com>
2023-12-10 00:58:02 +0100

    Change CoursePage to FinalizeOffer page

---
+++ frontend/src/pages/finalizedOffers/FinalizedOffers.tsx
@@ -0,0 +1,23 @@
+import { useQuery } from "react-query";
+import { withPrivateRoute } from "../../common/withPrivateRoute/WithPrivateRoute";
+import "./style.scss";
+import { getOffers } from "../../apiFunctions/getOffers";
+import { OfferStatus } from "../../models/OfferStatus";
+import { Loader } from "../../common/loader/Loader";
+
+function FinalizedOffers() {
+  const { data: finalizedOffers } = useQuery(["offers", "finalized"], () =>
+    getOffers(OfferStatus.COMPLETED)
+  );
+  return (
+    <section>
+      {finalizedOffers ? (
+        finalizedOffers.map((offer) => offer.my_course.course_name)
+      ) : (
+        <Loader />
+      )}
+    </section>
+  );
+}
+
+export const PrivateFinalizedOffers = withPrivateRoute(FinalizedOffers);
