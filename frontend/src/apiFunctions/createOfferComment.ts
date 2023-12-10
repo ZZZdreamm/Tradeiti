72c9ce7d52685d2276d8bc2f2daffbcb2582dd03
ZZZdream <kmultan1234@gmail.com>
2023-12-10 12:35:01 +0100

    Add creating comments

--- frontend/src/apiFunctions/createOfferComment.ts
+++ frontend/src/apiFunctions/createOfferComment.ts
@@ -1,5 +1,6 @@
 import { axiosBase } from "../config/axiosConfig";
+import { Comment } from "../models/Comment";
 
 export function createOfferComment(comment: Comment) {
-    return axiosBase.post("/comments/create", comment);
+  return axiosBase.post("/comments/create", comment);
 }
