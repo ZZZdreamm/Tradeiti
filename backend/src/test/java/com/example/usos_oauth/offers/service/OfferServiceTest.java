3ddc83757293d277549e0a1e39e16cc70f8c4181
oszypczy <01178608@pw.edu.pl>
2023-12-11 23:54:45 +0100

    backend: some OfferRepository and OfferService tests

---
+++ backend/src/test/java/com/example/usos_oauth/offers/service/OfferServiceTest.java
@@ -0,0 +1,36 @@
+package com.example.usos_oauth.offers.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OfferServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getOffer() {
+    }
+
+    @Test
+    void getAllOffers() {
+    }
+
+    @Test
+    void getOffersOfState() {
+    }
+
+    @Test
+    void getOffersWithUserOfState() {
+    }
+
+    @Test
+    void deleteOffer() {
+    }
+
+
+
+}
